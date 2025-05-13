e41b0c3f3fd3670fc952b51c94c1d77eb61947e2
August Levinson <118256272+920523@users.noreply.github.com>
2025-05-13 11:44:35 +0200

    Corrected tests according to feedback

--- backend/test/tests/test_dao_create.py
+++ backend/test/tests/test_dao_create.py
@@ -5,52 +5,60 @@ from unittest.mock import patch
 from src.util.dao import DAO
 
 
-
 class TestDaoCreate:
+    """
+    Test suite for DAO create
+    """
 
     @pytest.fixture
     def sut_user(self):
-        dao = DAO(collection_name="user", test_database=True)
-        object = dao.create({"firstName": "Test", "lastName": "Testson", "email": "test@test.se"})
+        """
+        Fixture for creating user DAO
+        """
+        dao = DAO(collection_name="testUser", test_database=True)
         try:
             yield dao
         finally:
             dao.drop()
-
 
     @pytest.fixture
     def sut_video(self):
-        dao = DAO(collection_name="video", test_database=True)
-        object = dao.create({"url": "https://url.com"})
+        """
+        Fixture for creating video DAO
+        """
+        dao = DAO(collection_name="testVideo", test_database=True)
         try:
             yield dao
         finally:
             dao.drop()
-
 
     def test_dao_create_invalid(self, sut_user):
         """
         Invalid format, required property lastName missing
         """
         with pytest.raises(pymongo.errors.WriteError):
-            sut_user.create(
-                {"firstName": "Test", "email": "test@test.se"}
-            ) 
+            sut_user.create({"firstName": "Test", "email": "test@test.se"})
 
     def test_dao_create_valid_unique(self, sut_user):
         """
         Valid format and unique email address should return object
         """
+        object = sut_user.create(
+            {"firstName": "Test", "lastName": "Testson", "email": "test@test.se"}
+        )
         obj = sut_user.create(
-                {"firstName": "Test", "lastName": "Testson", "email": "unique@test.se"}
-            )
-        
+            {"firstName": "Test", "lastName": "Testson", "email": "unique@test.se"}
+        )
+
         assert isinstance(obj, dict)
 
     def test_dao_create_valid_not_unique(self, sut_user):
         """
         Valid format but not unique email address should raise WriteError
         """
+        object = sut_user.create(
+            {"firstName": "Test", "lastName": "Testson", "email": "test@test.se"}
+        )
         with pytest.raises(pymongo.errors.WriteError):
             sut_user.create(
                 {"firstName": "Test", "lastName": "Testson", "email": "test@test.se"}
@@ -60,6 +68,14 @@ class TestDaoCreate:
         """
         Valid format and not unique url should return object
         """
+        object = sut_video.create({"url": "https://url.com"})
         obj = sut_video.create({"url": "https://url.com"})
-        
+
         assert isinstance(obj, dict)
+
+    def test_dao_create_video_invalid_type(self, sut_video):
+        """
+        Invalid type raises WriteError
+        """
+        with pytest.raises(pymongo.errors.WriteError):
+            obj = sut_video.create({"url": 42})
--- backend/test/tests/test_usercontroller.py
+++ backend/test/tests/test_usercontroller.py
@@ -3,29 +3,32 @@ import unittest.mock as mock
 
 from src.controllers.usercontroller import UserController
 
+
 def test_get_user_by_email_valid_unique():
     # Test if the function returns one user with an unique email.
     mockedDAO = mock.MagicMock()
 
-    mockedDAO.find.return_value = [{"user":"A user"}]
+    mockedDAO.find.return_value = [{"user": "A user"}]
 
     sut = UserController(dao=mockedDAO)
 
     result = sut.get_user_by_email(email="hi@hello.com")
 
-    assert result == {"user":"A user"}
+    assert result == {"user": "A user"}
+
 
 def test_get_user_by_email_valid_not_unique():
     # Test if the function returns one user with a not unique email. Should return the first user in the list.
     mockedDAO = mock.MagicMock()
 
-    mockedDAO.find.return_value = [{"user":"Another user"}, {"user":"A user"}]
+    mockedDAO.find.return_value = [{"user": "Another user"}, {"user": "A user"}]
 
     sut = UserController(dao=mockedDAO)
 
     result = sut.get_user_by_email(email="hi@hello.com")
 
-    assert result == {"user":"Another user"}
+    assert result == {"user": "Another user"}
+
 
 def test_get_user_by_email_valid_not_in_db():
     # Test if None is returned when an email is not in the database
@@ -39,16 +42,18 @@ def test_get_user_by_email_valid_not_in_db():
 
     assert result == None
 
+
 def test_get_user_by_email_invalid_email():
     # Assert that an invalid email raises a ValueError
     with pytest.raises(ValueError):
         mockedDAO = mock.MagicMock()
 
-        mockedDAO.find.return_value = [{"user":"A user"}]
+        mockedDAO.find.return_value = [{"user": "A user"}]
 
         sut = UserController(dao=mockedDAO)
 
         result = sut.get_user_by_email(email="hello.com")
+
 
 def test_get_user_by_email_db_fail():
     # Test an Exception is raised if the database fails
@@ -60,4 +65,3 @@ def test_get_user_by_email_db_fail():
         sut = UserController(dao=mockedDAO)
 
         result = sut.get_user_by_email(email="hi@hello.com")
-
